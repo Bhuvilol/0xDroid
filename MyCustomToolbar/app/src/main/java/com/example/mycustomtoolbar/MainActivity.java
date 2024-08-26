e81fe07836d59e9cd4348d616a32bd506b637a3b
Bhabesh <bhabeshcse@gmail.com>
2024-08-26 15:49:35 +0530

    Custom toolbar created

--- MyCustomToolbar/app/src/main/java/com/example/mycustomtoolbar/MainActivity.java
+++ MyCustomToolbar/app/src/main/java/com/example/mycustomtoolbar/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.mycustomtoolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("OxBhuvi");
+        }
+
+        toolbar.setSubtitle("Bhabesh Behera");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.opti_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.opt_new){
+            Toast.makeText(this, "Create New Model", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId()==R.id.opt_open) {
+            Toast.makeText(this, "Open a model", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId()==R.id.opt_rename) {
+            Toast.makeText(this, "Rename this model", Toast.LENGTH_SHORT).show();
+        } else {
+            super.onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
