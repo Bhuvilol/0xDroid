a4b85367dc0f48447dd4dbbb9a0f8f0adbb4d6d9
Bhabesh <bhabeshcse@gmail.com>
2024-08-29 18:59:41 +0530

    Dialog box created

--- MyDialogBox/app/src/main/java/com/example/mydialogbox/MainActivity.java
+++ MyDialogBox/app/src/main/java/com/example/mydialogbox/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.mydialogbox;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,6 +25,72 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Title")
+//                .setIcon(R.drawable.baseline_info_24)
+//                .setMessage("Message")
+//                .setNeutralButton("I have read", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(MainActivity.this, "Yes , you can proceed now", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//        AlertDialog dialog1 = builder.create();
+//        dialog1.show();
+
+        AlertDialog.Builder delDialog = new AlertDialog.Builder(MainActivity.this);
+        delDialog.setTitle("Delete")
+                .setIcon(R.drawable.baseline_delete_forever_24)
+                .setMessage("Sure delete?")
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Deleted Succesfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                delDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Not deleted", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                delDialog.show();
+
+
+
+
+//        AlertDialog dialog2 = builder.create();
+//        dialog2.show();
+
     }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder exit = new AlertDialog.Builder(this);
+        exit.setTitle("Exit?")
+                .setMessage("Are you sure?")
+                .setIcon(R.drawable.baseline_exit_to_app_24);
+        exit.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Welcome back", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        exit.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MainActivity.super.onBackPressed();
+            }
+        });
+        exit.show();
+//        super.onBackPressed();
+    }
+
+
 }
