aa8b0efe48ec86ec720bfc96fe2882d5dab0ceb8
Bhabesh <bhabeshcse@gmail.com>
2024-08-30 09:01:15 +0530

    Fragments learmed

---
+++ Fragments/app/src/main/java/com/example/fragments/BFragment.java
@@ -0,0 +1,26 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class BFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_b, container, false);
+        TextView textview = view.findViewById(R.id.bFrag);
+        //Set of instructions below
+
+
+        return view;
+    }
+}
--- Fragments/app/src/main/java/com/example/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.fragments;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnFragA,btnFragB,btnFragC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnFragA = findViewById(R.id.btnFragA);
+        btnFragB = findViewById(R.id.btnFragB);
+        btnFragC = findViewById(R.id.btnFragC);
+
+        //default frag
+        loadFragment(new BFragment(),0);
+
+        btnFragA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new AFragment(),1);
+            }
+        });
+        btnFragB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new BFragment(),2);
+            }
+        });
+        btnFragC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new CFragment(),3);
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fragment ,int flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if (flag == 0){
+            ft.add(R.id.container,fragment);
+        } else {
+            ft.replace(R.id.container,fragment);
+        }
+
+        ft.commit();
+
     }
 }
